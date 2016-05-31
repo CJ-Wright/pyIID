4dfd11937393ff481a000508e3f6a4fcfae55237
CJ-Wright <cjwright4242@gmail.com>
2016-05-31 13:50:11 -0400

    BLD: misspelled packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_pacakges
+from setuptools import setup, find_packages
 # from distutils.core import setup
 
 setup(
     name='pyIID',
     version='',
-    packages=find_pacakges(),
+    packages=find_packages(),
     url='https://github.com/ZhouHUB/pyIID',
     license='',
     author='christopher',
